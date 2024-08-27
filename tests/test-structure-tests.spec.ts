912c4d1b60d5872dd69e7e20b022e31099fb180b
Petr Fifka <petrfifk@gmail.com>
2024-08-27 14:32:47 +0200

    change trace and add tag github

--- tests/test-structure-tests.spec.ts
+++ tests/test-structure-tests.spec.ts
@@ -1,23 +1,29 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("PMTool Login Tests", () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto("https://tredgate.com/pmtool/");
-  });
+test.describe(
+  "PMTool Login Tests",
+  {
+    tag: "@github",
+  },
+  () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto("https://tredgate.com/pmtool/");
+    });
 
-  test("Successful Login", async ({ page }) => {
-    await page.locator("#username").fill("pw_skoleni");
-    await page.locator("#password").fill("TEG2023");
-    await page.locator('//button[@type="submit"]').click();
-    await expect(page.locator("#welcome-page-header")).toContainText(
-      "Vítej v testovací aplikaci"
-    );
-  });
+    test("Successful Login", async ({ page }) => {
+      await page.locator("#username").fill("pw_skoleni");
+      await page.locator("#password").fill("TEG2023");
+      await page.locator('//button[@type="submit"]').click();
+      await expect(page.locator("#welcome-page-header")).toContainText(
+        "Vítej v testovací aplikaci"
+      );
+    });
 
-  test("Failed Login ", async ({ page }) => {
-    await page.locator("#username").fill("1234");
-    await page.locator("#password").fill("5432");
-    await page.locator('//button[@type="submit"]').click();
-    await expect(page.locator(".alert-danger")).toBeVisible();
-  });
-});
+    test("Failed Login ", async ({ page }) => {
+      await page.locator("#username").fill("1234");
+      await page.locator("#password").fill("5432");
+      await page.locator('//button[@type="submit"]').click();
+      await expect(page.locator(".alert-danger")).toBeVisible();
+    });
+  }
+);
